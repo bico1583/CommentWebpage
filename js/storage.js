21f2f7211c8c661c4b0f0a7d41d6aede2ddef84b
BICO <bico1583@gmail.com>
2026-09-22 00:32:59 +0200

    wrote out functions in middleman file

--- js/storage.js
+++ js/storage.js
@@ -4,24 +4,60 @@ const CURRENT_USER_STORAGE_KEY = "comments_current_user";
 
 
 function getUsers() {
+    const users = localStorage.getItem(USERS_STORAGE_KEY);
+
+    if (!users) {
+        return [];
+    }
+
+    return JSON.parse(users);
 }
 
 
 function saveUsers(users) {
+    localStorage.setItem(
+        USERS_STORAGE_KEY,
+        JSON.stringify(users)
+    );
 }
 
 
 function getComments() {
+    const comments = localStorage.getItem(COMMENTS_STORAGE_KEY);
+
+    if (!comments) {
+        return [];
+    }
+
+    return JSON.parse(comments);
 }
 
 
 function saveComments(comments) {
+    localStorage.setItem(
+        COMMENTS_STORAGE_KEY,
+        JSON.stringify(comments)
+    );
 }
 
 
 function getCurrentUser() {
+    return sessionStorage.getItem(
+        CURRENT_USER_STORAGE_KEY
+    );
 }
 
 
 function setCurrentUser(username) {
+    sessionStorage.setItem(
+        CURRENT_USER_STORAGE_KEY,
+        username
+    );
+}
+
+
+function clearCurrentUser() {
+    sessionStorage.removeItem(
+        CURRENT_USER_STORAGE_KEY
+    );
 }
