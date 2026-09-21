694cc9d45cf840925242ae858c9aded6d1974c56
BICO <bico1583@gmail.com>
2026-09-22 00:27:20 +0200

    storage functions

---
+++ js/storage.js
@@ -0,0 +1,27 @@
+const USERS_STORAGE_KEY = "comments_users";
+const COMMENTS_STORAGE_KEY = "comments";
+const CURRENT_USER_STORAGE_KEY = "comments_current_user";
+
+
+function getUsers() {
+}
+
+
+function saveUsers(users) {
+}
+
+
+function getComments() {
+}
+
+
+function saveComments(comments) {
+}
+
+
+function getCurrentUser() {
+}
+
+
+function setCurrentUser(username) {
+}
